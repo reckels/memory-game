ee6e8ee317e792506ab27ee4393281011bf9864c
Rachel Eckels <Rachel.Eckels@walmart.com>
2022-02-02 21:02:25 -0600

    Finish Part 1

---
+++ memory-game/src/components/MemoryCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MemoryCard.css';
+
+class MemoryCard extends React.Component {
+    render(){
+        return(
+            <div class="MemoryCard">
+                <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="logo" />
+            </div>
+        )
+        
+    }
+}
+
+export default MemoryCard;
