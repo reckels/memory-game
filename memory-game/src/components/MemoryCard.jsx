48eb6e7d1bf277446e01ca7292b5150e709f8440
Rachel Eckels <Rachel.Eckels@walmart.com>
2022-02-03 10:51:13 -0600

    Finish Part 2

--- memory-game/src/components/MemoryCard.jsx
+++ memory-game/src/components/MemoryCard.jsx
@@ -4,17 +4,23 @@ import './MemoryCard.css';
 class MemoryCard extends React.Component {
     constructor() {
         super();
-        this.state = {isFlipped: false};
+        this.state = {isFlipped: false,};
     }
-    clickHandler() {
-        this.setState(({isFlipped}) => ({
-            isFlipped: !isFlipped
-        }));
+    clickHandler = () => {
+        this.setState(previousState => {
+            return{
+                isFlipped: !previousState.isFlipped,
+            }
+        });
     }
     render(){
+        let memoryCardInnerClass = "MemoryCardInner";
+        if (this.state.isFlipped) {
+            memoryCardInnerClass = "MemoryCardInner Flipped";
+        }
         return(
             <div className="MemoryCard" onClick={this.clickHandler}>
-                <div className="MemoryCardInner">
+                <div className={memoryCardInnerClass}>
                     <div className="MemoryCardBack">
                         <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" alt="logo" />
                     </div>
