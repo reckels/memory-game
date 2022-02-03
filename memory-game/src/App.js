136f4a6e47df6cda834a027680fda0b2efdcb1cb
Rachel Eckels <Rachel.Eckels@walmart.com>
2022-02-03 12:19:52 -0600

    start logic

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,6 +1,27 @@
+import React from 'react';
 import './App.css';
 import MemoryCard from './components/MemoryCard';
 
+function generateDeck() {
+  const symbols = [Δ, β, Ψ, δ, λ, Σ, Φ, Ω];
+  deck = [];
+  for (let i = 0; i < 16; i++){
+    deck.push(card = {isFlipped: false, symbol: symbols[i % 8]})
+  }
+  shuffle(deck);
+  return deck;
+}
+
+function shuffle(deck) {
+  var j, x, i;
+  for (i = deck.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = deck[i];
+      deck[i] = deck[j];
+      deck[j] = x;
+  }
+  return deck;
+}
 
 function App() {
   return (
